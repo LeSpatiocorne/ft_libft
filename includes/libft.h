4ead3ba4776dfab857e9dba67ac628b0a25e8b88
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2025-06-28 21:40:27 +0200

    added ansi color codes

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nidruon <nidruon@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 18:19:26 by nidruon           #+#    #+#             */
-/*   Updated: 2025/06/28 21:24:51 by nidruon          ###   ########.fr       */
+/*   Updated: 2025/06/28 21:39:47 by nidruon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,84 @@
 # include <stdarg.h>
 # include <unistd.h>
 
+// Default buffer size for get_next_line
 # define BUFFER_SIZE 42
 
+/* -------------------------- ANSI MARKDOWN -------------------------- */
+# define RESET "\033[0m"
+// regular colors
+# define BLACK "\033[30m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define PURPLE "\033[35m"
+# define CYAN "\033[36m"
+# define WHITE "\033[37m"
+// bold colors
+# define BOLD_BLACK "\033[30;1m"
+# define BOLD_RED "\033[31;1m"
+# define BOLD_GREEN "\033[32;1m"
+# define BOLD_YELLOW "\033[33;1m"
+# define BOLD_BLUE "\033[34;1m"
+# define BOLD_PURPLE "\033[35;1m"
+# define BOLD_CYAN "\033[36;1m"
+# define BOLD_WHITE "\033[37;1m"
+// underline colors
+# define UNDERLINE_BLACK "\033[30;4m"
+# define UNDERLINE_RED "\033[31;4m"
+# define UNDERLINE_GREEN "\033[32;4m"
+# define UNDERLINE_YELLOW "\033[33;4m"
+# define UNDERLINE_BLUE "\033[34;4m"
+# define UNDERLINE_PURPLE "\033[35;4m"
+# define UNDERLINE_CYAN "\033[36;4m"
+# define UNDERLINE_WHITE "\033[37;4m"
+// background colors
+# define BACKGROUND_BLACK "\033[40m"
+# define BACKGROUND_RED "\033[41m"
+# define BACKGROUND_GREEN "\033[42m"
+# define BACKGROUND_YELLOW "\033[43m"
+# define BACKGROUND_BLUE "\033[44m"
+# define BACKGROUND_PURPLE "\033[45m"
+# define BACKGROUND_CYAN "\033[46m"
+# define BACKGROUND_WHITE "\033[47m"
+// High Intensity colors
+# define HIGH_INTENSITY_BLACK "\033[90m"
+# define HIGH_INTENSITY_RED "\033[91m"
+# define HIGH_INTENSITY_GREEN "\033[92m"
+# define HIGH_INTENSITY_YELLOW "\033[93m"
+# define HIGH_INTENSITY_BLUE "\033[94m"
+# define HIGH_INTENSITY_PURPLE "\033[95m"
+# define HIGH_INTENSITY_CYAN "\033[96m"
+# define HIGH_INTENSITY_WHITE "\033[97m"
+// Bold High Intensity colors
+# define BOLD_HIGH_INTENSITY_BLACK "\033[90;1m"
+# define BOLD_HIGH_INTENSITY_RED "\033[91;1m"
+# define BOLD_HIGH_INTENSITY_GREEN "\033[92;1m"
+# define BOLD_HIGH_INTENSITY_YELLOW "\033[93;1m"
+# define BOLD_HIGH_INTENSITY_BLUE "\033[94;1m"
+# define BOLD_HIGH_INTENSITY_PURPLE "\033[95;1m"
+# define BOLD_HIGH_INTENSITY_CYAN "\033[96;1m"
+# define BOLD_HIGH_INTENSITY_WHITE "\033[97;1m"
+// Underline High Intensity colors
+# define UNDERLINE_HIGH_INTENSITY_BLACK "\033[90;4m"
+# define UNDERLINE_HIGH_INTENSITY_RED "\033[91;4m"
+# define UNDERLINE_HIGH_INTENSITY_GREEN "\033[92;4m"
+# define UNDERLINE_HIGH_INTENSITY_YELLOW "\033[93;4m"
+# define UNDERLINE_HIGH_INTENSITY_BLUE "\033[94;4m"
+# define UNDERLINE_HIGH_INTENSITY_PURPLE "\033[95;4m"
+# define UNDERLINE_HIGH_INTENSITY_CYAN "\033[96;4m"
+# define UNDERLINE_HIGH_INTENSITY_WHITE "\033[97;4m"
+// Background High Intensity colors
+# define BACKGROUND_HIGH_INTENSITY_BLACK "\033[90;40m"
+# define BACKGROUND_HIGH_INTENSITY_RED "\033[91;41m"
+# define BACKGROUND_HIGH_INTENSITY_GREEN "\033[92;42m"
+# define BACKGROUND_HIGH_INTENSITY_YELLOW "\033[93;43m"
+# define BACKGROUND_HIGH_INTENSITY_BLUE "\033[94;44m"
+# define BACKGROUND_HIGH_INTENSITY_PURPLE "\033[95;45m"
+# define BACKGROUND_HIGH_INTENSITY_CYAN "\033[96;46m"
+# define BACKGROUND_HIGH_INTENSITY_WHITE "\033[97;47m"
+/* -------------------------- BASE FUNCTIONS -------------------------- */
 void	ft_putchar(char c);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
@@ -63,9 +139,9 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	ft_putnbr_base_fd(unsigned long nb, char *base, int fd);
-
+/* -------------------------- GET_NEXT_LINE --------------------------- */
 char	*get_next_line(int fd);
-
+/* --------------------------- FT_PRINTF ------------------------------ */
 int		ft_print_char(va_list *args);
 int		ft_print_hex(va_list *args, char format);
 int		ft_print_int(va_list *args);
